8aab561a8aa8c9de0add87523bac5ab2fef86fb7
Fesposito9197 <kekkoesp8@gmail.com>
2023-03-15 15:16:06 +0100

    aggiunge stampe a video del array myfavoritefood

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] myFavoriteFood = {"pasta al pesto","pasta al sugo","patatine fritte","hamburger","pizza"};
+//Stampo la lunghezza del array
+Console.WriteLine(myFavoriteFood.Length);
+
+for (int i = 0; i < myFavoriteFood.Length; i++)
+{
+    //stampo per intero la lista del array 
+    Console.WriteLine(myFavoriteFood[i]);
+}
+
+//stampo il prima in classifica 
+Console.WriteLine("La prima classificata e' " + myFavoriteFood[0]);
+//stampo l'ultima in classifica
+Console.WriteLine("L'ultima classificata e' " + myFavoriteFood[myFavoriteFood.Length - 1]);
+//stampo la meta classifica 
+Console.WriteLine("A meta classifica c'e " + myFavoriteFood[myFavoriteFood.Length / 2]);
+
